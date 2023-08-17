a52bd9f295e1ea463accb49ce9ac3b2f31a2c0c0
Esteban <juanesbs2003@hotmail.com>
2023-08-17 18:03:00 -0500

    corrigiendo el carousel de botones

--- src/app/shared/button-carousel/button-carousel.component.ts
+++ src/app/shared/button-carousel/button-carousel.component.ts
@@ -1,28 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import Swiper from 'swiper';
+
 
 @Component({
   selector: 'app-button-carousel',
   templateUrl: './button-carousel.component.html',
   styleUrls: ['./button-carousel.component.css']
 })
-export class ButtonCarouselComponent implements OnInit{
+export class ButtonCarouselComponent {
 
-  showCarousel = false;
-
-  ngOnInit(): void {
-    if (window.innerWidth < 1024) { // Cambia 1024 por el ancho deseado para activar el carrusel
-      this.showCarousel = true;
-
-      const swiper = new Swiper('.swiper-container', {
-        slidesPerView: 'auto',
-        spaceBetween: 5,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-    }
-  }
 
 }
