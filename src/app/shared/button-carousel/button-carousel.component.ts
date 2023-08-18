257a8f18293e699fce16e84580cd1749b1b638a5
Esteban <juanesbs2003@hotmail.com>
2023-08-18 18:15:55 -0500

    añadiendo responsive a button carrousel

--- src/app/shared/button-carousel/button-carousel.component.ts
+++ src/app/shared/button-carousel/button-carousel.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  AfterViewInit, HostListener  } from '@angular/core';
+
+const carrousel = {
 
 
+}
 @Component({
   selector: 'app-button-carousel',
   templateUrl: './button-carousel.component.html',
   styleUrls: ['./button-carousel.component.css']
 })
+
+
+
+
 export class ButtonCarouselComponent {
+
+  cerrado: boolean = false;
+
+
+  screenWidth: number = window.innerWidth;
+
+
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.screenWidth = window.innerWidth;
+    console.log(this.screenWidth);
+    if(this.screenWidth <=874){
+      this.cerrado = true;
+    }
+    else{
+      this.cerrado = false;
+    }
+
+  }
+
+
+
+
 
 
 }
+
+
