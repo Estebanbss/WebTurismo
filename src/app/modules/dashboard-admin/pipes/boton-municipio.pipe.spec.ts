8049697730ecca0e97bdd15a8aba42dc8c62e07b
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2023-10-31 22:08:46 -0500

    borrando tests (son inutiles)

--- src/app/modules/dashboard-admin/pipes/boton-municipio.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BotonMunicipioPipe } from './boton-municipio.pipe';
-
-describe('BotonMunicipioPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BotonMunicipioPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
