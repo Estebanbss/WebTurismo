b9709deae4d527bd5fa656661438b28d43ef61d4
Esteban <112998820+Estebanbss@users.noreply.github.com>
2023-11-07 10:11:39 -0500

    quitando pendejadas

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -13,9 +13,6 @@ import { BehaviorSubject } from 'rxjs';
 
 
 export class UserService {
-  getDoc(docuRef: DocumentReference<DocumentData, DocumentData>) {
-    throw new Error('Method not implemented.');
-  }
 
   infoUsuario: any;
 
