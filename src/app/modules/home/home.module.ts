1c4efc507e8fb3f4746b9ca4ac0f150dc97f836b
Santiago Vargas N <90786097+Sanvargas02@users.noreply.github.com>
2023-12-04 12:01:42 -0500

    Correos electrónicos listos

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgOptimizedImage } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
+import { ReactiveFormsModule } from '@angular/forms'; //Formularios Reactivos
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -24,7 +25,8 @@ import {ScrollingModule}from '@angular/cdk/scrolling'
     HomeRoutingModule,
     NgOptimizedImage,
     SharedModule,
-    ScrollingModule
+    ScrollingModule,
+    ReactiveFormsModule
 
 
 
